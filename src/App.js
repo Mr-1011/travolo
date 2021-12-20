d0956859e396d80055250a9e2df05dd6868f8b6b
Mr1011 <furkan.imamoglu@tum.de>
2021-12-20 13:00:40 +0100

    first packages installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from './Components/Navbar'
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
 
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import HelpIcon from '@mui/icons-material/Help';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
