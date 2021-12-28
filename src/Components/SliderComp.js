aae7a0fa3f4452e982a1760e673b2c7c09ca8d07
Mr1011 <furkan.imamoglu@tum.de>
2021-12-28 18:35:51 +0100

    slider hotfix

--- src/Components/SliderComp.js
+++ src/Components/SliderComp.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from '@mui/material/Slider';
 import '../Styling/Sliders.css';
 
 export default function SliderComp({ title, svg }) {
 
+  const [value, setValue] = useState(50);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className='slider__component'>
       <div className='slider__info'>
-        <h1>{title}</h1>
+        <h1 className='slider__title'>{title}</h1>
         <img src={svg} className='slider__svg ' alt='' />
       </div>
       <div className='slider__div'>
         <Slider
-          value={50}
+          value={value}
+          onChange={handleChange}
           sx={{
             width: 250,
             color: 'white',
---
+++ src/Data/data.js
@@ -0,0 +1,14 @@
+const pics = [
+  {
+    id: 1,
+    img: '../Data/pictures/Architektur 1.jpg',
+    desc: 'Architectural building',
+  },
+  {
+    id: 2,
+    img: './pictures/Architektur 2.jpg',
+    desc: 'Architectural building',
+  }
+]
+
+export default pics;
